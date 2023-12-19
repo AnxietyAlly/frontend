394360200a379585ce141c32500ed29dff95ceac
juliapieters <piet0058@hz.nl>
2023-12-19 11:52:11 +0100

    login page layout fix

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,17 +4,38 @@
 </script>
 
 <div class="container">
-	<h1 class="is-size-3 has-text-weight-semibold my-4">Login or Register</h1>
-	<form method="post" use:enhance>
-		<input class="input my-2" type="text" placeholder="Username" name="name" required />
-		<input class="input my-2" type="text" placeholder="Email" name="email" required />
-		<input class="input my-2" type="password" placeholder="Password" name="password" required />
+	<div class="flex flex-col items-center">
+		<img class="w-40 h-100 mt-4" src="/anxietyally.png" alt="Anxiety Ally Logo" />
+	</div>
+	<div class="h-10 mt-2 flex justify-center mb-2">
+		<h1 class="text-3xl font-bold text-red-400 text-opacity-90">Login or Register</h1>
+	</div>
+	<form class="h-10 mt-6" method="post" use:enhance>
+		<div class="flex justify-center">
+			<div class="w-5/6 md:w-1/2 lg:w-1/4 bg-white bg-opacity-50 p-4 rounded-md">
+				<label class="text-xl font-bold text-red-400">Username</label>
+				<input class="input my-2 border border-white" type="text" placeholder="" name="name" required />
+				<label class="text-xl font-bold text-red-400">E-mail</label>
+				<input class="input my-2 border border-white" type="text" placeholder="" name="email" required />
+				<label class="text-xl font-bold text-red-400">Password</label>
+				<input class="input my-2 border border-white" type="password" placeholder="" name="password" required />
 
-		{#if form?.errorMessage}
-			<div class="has-text-danger my-2">{form.errorMessage}</div>
-		{/if}
-
-		<button class="button mt-4 mr-3" type="submit" formaction="?/register">Register</button>
-		<button class="button is-primary mt-4" type="submit" formaction="?/login">Login</button>
+				{#if form?.errorMessage}
+					<div class="has-text-danger my-2">{form.errorMessage}</div>
+				{/if}
+			</div>
+		</div>
+		<div class="flex justify-center items-center mt-14">
+			<button
+				class="w-40 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium mr-3 rounded-3xl shadow col-span-2 bg-red-400 bg-opacity-70 text-white md:py-4 md:text-lg md:px-10"
+				type="submit"
+				formaction="?/register">Register</button
+			>
+			<button
+			class="w-40 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium mr-3 rounded-3xl shadow col-span-2 bg-red-400 bg-opacity-70 text-white md:py-4 md:text-lg md:px-10"
+				type="submit"
+				formaction="?/login">Login</button
+			>
+		</div>
 	</form>
 </div>
