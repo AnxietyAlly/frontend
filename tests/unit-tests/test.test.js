c0fcf4f3064073562dd33eb5b9312308c5bb3959
DonDuck1 <malj0042@hz.nl>
2024-01-09 15:20:10 +0100

    Added some tests, Jest still doesn't seem to like to work with svelte

---
+++ tests/unit-tests/test.test.js
@@ -0,0 +1,49 @@
+const sessionStoreJavascript = require('../../src/lib/server/sessionStore/index');
+const createSession = sessionStoreJavascript.createSession;
+const deleteSession = sessionStoreJavascript.deleteSession;
+const getSession = sessionStoreJavascript.getSession;
+
+const maxAge1 = 180;
+const testSession1 = createSession("testSession1", maxAge1);
+const invalidAt = Date.now() + maxAge1;
+
+const maxAge2 = 180;
+const testSession2 = createSession("sessionToBeDeleted", maxAge2);
+
+test("createSession has made a session", () => {
+    expect(testSession1).toBeDefined();
+});
+
+test("createSession's sid is 64 characters long", () => {
+    expect(testSession1).toHaveLength(64);
+});
+
+test("getSession returns a name", () => {
+    expect(getSession(testSession1).name).toBeDefined();
+});
+
+test("getSession returns the maxAge", () => {
+    expect(getSession(testSession1).invalidAt).toBeDefined();
+});
+
+test("createSession's name is correct", () => {
+    expect(getSession(testSession1).name).toBe("testSession1");
+});
+
+test("createSession's maxAge is correct", () => {
+    expect(getSession(testSession1).invalidAt - invalidAt).toBeLessThanOrEqual(5);
+});
+
+deleteSession(testSession2);
+test("deleteSession deleted the session", () => {
+    expect(getSession(testSession2)).toBeUndefined();
+});
+
+// import Account from '../../src/lib/components/Account.svelte';
+// import { render, screen } from '@testing-library/svelte';
+
+// test("says '...waiting'", () => {
+// 	render(Account);
+// 	const node = screen.queryByText("...waiting");
+// 	expect(node).not.toBeNull();
+// });
--- unit-tests/test.test.js
+++
@@ -1,7 +0,0 @@
-function sum(a, b) {
-	return a + b;
-}
-
-test('1 plus 2', () => {
-	expect(sum(1, 2)).toBe(3);
-});
