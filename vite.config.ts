adbd986cab8f2958ade099eb21b0be4e4d139c57
DonDuck1 <malj0042@hz.nl>
2024-01-09 16:19:56 +0100

    Can now make coverage file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit(), purgeCss()],
 	test: {
+		coverage: {
+		  	provider: 'v8', // or 'istanbul', 'v8' is default
+			reporter: ['cobertura', 'text'],
+		},
 		/* for example, use global to avoid globals imports (describe, test, expect): */
 		// globals: true,
 	},
